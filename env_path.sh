64d3a863a8a2b5808279886f517d7c168055fdcc
bba753951 <bba753951@gmail.com>
2020-07-23 22:34:11 +0800

    modified path

--- env_path.sh
+++ env_path.sh
@@ -1,16 +1,17 @@
 
-export PATH=$PATH:/home/bba753951/bio_tool/bowtie-1.2.3-linux-x86_64
+export PATH=/home/bba753951/bio_tool/bowtie-1.2.3-linux-x86_64:$PATH
 export PATH=$PATH:/home/bba753951/bio_tool/bowtie2-2.4.1-linux-x86_64
 export PATH=$PATH:/home/bba753951/bio_tool/ViennaRNA/bin
 export PATH=$PATH:/home/bba753951/bio_tool/FastQC
 export PATH=$PATH:/home/bba753951/bio_tool/TrimGalore-0.6.5
 # blatsrc
-export PATH=$PATH:/home/bba753951/bin/x86_64
+#export PATH=$PATH:/home/bba753951/bin/x86_64
 
-export PATH=$PATH:/home/bba753951/master_final/bin
 # hyb
-export PATH=$PATH:/home/bba753951/pra_hyb/hyb-master/bin
+#export PATH=$PATH:/home/bba753951/pra_hyb/hyb-master/bin
 # CLAN
-export PATH=$PATH:/home/bba753951/pra_clan/CLAN_release/bin
+#export PATH=$PATH:/home/bba753951/pra_clan/CLAN_release/bin
 
+
+# for fastx
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
